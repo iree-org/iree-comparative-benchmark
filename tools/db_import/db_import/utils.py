c9709cafecbdc66308eceaea023bc62adb5d613f
Henning Becker <henning.becker@gmail.com>
2023-06-13 08:15:30 +0000

    Add Jsonnet rule engine for DB import
    This is adding a Jsonnet-based rule engine for structured data
    transformation.
    
    We will use that to read benchmark results from GCS files (JSON or CSV
    or anything else) and transform this into tabular data for a database
    import.

---
+++ tools/db_import/db_import/utils.py
@@ -0,0 +1,26 @@
+## Copyright 2023 The OpenXLA Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from typing import TypeVar, Callable, Iterable
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+# Returns `function(item)` for the first `item` from `iterable` that didn't raise an exception.
+# If all items raise an exception the exception raised by `function(last_item)` is raised.
+# If the iterable yields no items, `None` is returned.
+def first_no_except(function: Callable[[T], R], iterable: Iterable[T]) -> R:
+  last_exception = None
+
+  for el in iterable:
+    try:
+      return function(el)
+    except Exception as e:
+      last_exception = e
+
+  if last_exception:
+    raise last_exception
---
+++ tools/db_import/tests/test_utils.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+## Copyright 2023 The OpenXLA Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import unittest
+from db_import.utils import first_no_except
+
+
+class TestFirstNoExcept(unittest.TestCase):
+
+  def test_empty(self):
+
+    def raise_immediately(arg):
+      raise RuntimeError("")
+
+    self.assertEqual(first_no_except(raise_immediately, []), None)
+
+  def test_failure(self):
+
+    def raise_immediately(arg):
+      raise RuntimeError("")
+
+    with self.assertRaises(RuntimeError):
+      first_no_except(raise_immediately, [1])
+
+  def test_success(self):
+
+    def raise_when_smaller_5(arg):
+      raise_when_smaller_5.number_of_calls += 1
+      if arg < 5:
+        raise RuntimeError()
+
+      return arg
+
+    raise_when_smaller_5.number_of_calls = 0
+
+    self.assertEqual(first_no_except(raise_when_smaller_5, range(10)), 5)
+    self.assertEqual(raise_when_smaller_5.number_of_calls, 6)
+
+
+if __name__ == "__main__":
+  unittest.main()
