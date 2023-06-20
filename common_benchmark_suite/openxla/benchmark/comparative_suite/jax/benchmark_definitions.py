8a2e1aece0e5d4d7373c7115cd7eceb49899cfc3
Jerry Wu <cheyuw@google.com>
2023-06-20 21:11:22 +0000

    Refactor

--- common_benchmark_suite/openxla/benchmark/comparative_suite/jax/benchmark_definitions.py
+++ common_benchmark_suite/openxla/benchmark/comparative_suite/jax/benchmark_definitions.py
@@ -13,18 +13,16 @@ from openxla.benchmark.comparative_suite.jax import model_definitions, test_data
 
 T5_LARGE_JAX_A2_HIGHGPU_1G = utils.build_batch_benchmark_cases(
     batch_models=model_definitions.T5_LARGE_FP32_JAX_512XI32_BATCHES,
-    batch_input_data=test_data_definitions.
-    T5_LARGE_JAX_SEQLEN512_I32_INPUT_BATCHES,
-    batch_output_data=test_data_definitions.
+    batch_inputs=test_data_definitions.T5_LARGE_JAX_SEQLEN512_I32_INPUT_BATCHES,
+    batch_expected_outputs=test_data_definitions.
     T5_LARGE_FP32_JAX_512X1024XF32_OUTPUT_BATCHES,
     target_device=gcp_devices.GCP_A2_HIGHGPU_1G,
     batch_sizes=[1, 16, 24, 32, 48, 64, 512],
 )
 T5_LARGE_JAX_C2_STANDARD_16 = utils.build_batch_benchmark_cases(
     batch_models=model_definitions.T5_LARGE_FP32_JAX_512XI32_BATCHES,
-    batch_input_data=test_data_definitions.
-    T5_LARGE_JAX_SEQLEN512_I32_INPUT_BATCHES,
-    batch_output_data=test_data_definitions.
+    batch_inputs=test_data_definitions.T5_LARGE_JAX_SEQLEN512_I32_INPUT_BATCHES,
+    batch_expected_outputs=test_data_definitions.
     T5_LARGE_FP32_JAX_512X1024XF32_OUTPUT_BATCHES,
     target_device=gcp_devices.GCP_C2_STANDARD_16,
     batch_sizes=[1, 16, 24, 32, 48, 64, 512],
