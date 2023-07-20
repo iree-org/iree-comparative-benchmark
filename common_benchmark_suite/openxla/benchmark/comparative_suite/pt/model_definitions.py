f1c586ec0813c3ed53cfe324a63bb9d6a6c05780
Jerry Wu <cheyuw@google.com>
2023-07-20 22:14:12 +0000

    2. Add model definition

--- common_benchmark_suite/openxla/benchmark/comparative_suite/pt/model_definitions.py
+++ common_benchmark_suite/openxla/benchmark/comparative_suite/pt/model_definitions.py
@@ -13,6 +13,26 @@ from openxla.benchmark.comparative_suite import utils
 PARENT_GCS_DIR = "https://storage.googleapis.com/iree-model-artifacts/pytorch/pt_models_20230709.894_1688992116/"
 ARTIFACTS_DIR_URL_TEMPLATE = string.Template(PARENT_GCS_DIR + "${name}")
 
+# Example model.
+EXAMPLE_PT_IMPL = def_types.ModelImplementation(
+    name="EXAMPLE_PT",
+    tags=["example"],
+    framework_type=def_types.ModelFrameworkType.PYTORCH,
+    module_path=f"{utils.MODELS_MODULE_PATH}.pt.example.example_model",
+    source_info="https://pytorch.org/vision/stable/models/mobilenetv3.html",
+)
+EXAMPLE_FP32_PT_BATCH_TEMPLATE = utils.ModelTemplate(
+    name=utils.BATCH_NAME("EXAMPLE_FP32_PT"),
+    tags=[utils.BATCH_TAG],
+    model_impl=EXAMPLE_PT_IMPL,
+    model_parameters={
+        "batch_size": utils.BATCH_SIZE_PARAM,
+        "data_type": "fp32",
+    },
+)
+EXAMPLE_FP32_PT_BATCHES = utils.build_batch_models(
+    template=EXAMPLE_FP32_PT_BATCH_TEMPLATE, batch_sizes=[1, 16, 32])
+
 # Bert models.
 # Model implementation from https://huggingface.co/docs/transformers/model_doc/bert#transformers.BertModel.
 # Batch sizes from MLPerf A100 Configs: https://github.com/mlcommons/inference_results_v2.1/tree/master/closed/NVIDIA/configs/bert
@@ -125,6 +145,7 @@ RESNET50_FP16_PT_3X224X224XF16_BATCHES = utils.build_batch_models(
 
 ALL_MODELS = list(
     itertools.chain(
+        EXAMPLE_FP32_PT_BATCHES.values(),
         BERT_LARGE_FP32_PT_384XI32_BATCHES.values(),
         BERT_LARGE_FP16_PT_384XI32_BATCHES.values(),
         RESNET50_FP32_PT_3X224X224XF32_BATCHES.values(),
