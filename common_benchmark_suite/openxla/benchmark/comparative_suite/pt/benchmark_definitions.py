d26445e66f747a2fa4972a14c6c97b6f583cc476
Jerry Wu <cheyuw@google.com>
2023-07-20 22:17:51 +0000

    Step 3. Define benchmark cases

--- common_benchmark_suite/openxla/benchmark/comparative_suite/pt/benchmark_definitions.py
+++ common_benchmark_suite/openxla/benchmark/comparative_suite/pt/benchmark_definitions.py
@@ -9,6 +9,12 @@ import itertools
 from openxla.benchmark.comparative_suite import utils
 from openxla.benchmark.comparative_suite.pt import model_definitions
 
+# Example benchmarks.
+EXAMPLE_FP32_PT_CASES = utils.build_batch_benchmark_cases(
+    batch_models=model_definitions.EXAMPLE_FP32_PT_BATCHES,
+    batch_sizes=[1, 16, 32],
+)
+
 BERT_LARGE_FP32_PT_384XI32_CASES = utils.build_batch_benchmark_cases(
     batch_models=model_definitions.BERT_LARGE_FP32_PT_384XI32_BATCHES,
     verify_parameters={
@@ -40,6 +46,7 @@ RESNET50_FP16_PT_3X224X224XF16_CASES = utils.build_batch_benchmark_cases(
 
 ALL_BENCHMARKS = list(
     itertools.chain(
+        EXAMPLE_FP32_PT_CASES.values(),
         BERT_LARGE_FP32_PT_384XI32_CASES.values(),
         BERT_LARGE_FP16_PT_384XI32_CASES.values(),
         RESNET50_FP32_PT_3X224X224XF32_CASES.values(),
