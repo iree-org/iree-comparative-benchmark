fc9d78bd352cdfb1252efcca3df16a4e60f62aee
mariecwhite <mariewhite@google.com>
2023-12-14 05:32:03 +0000

    Update Pixel 8 Pro CPU feature flags

--- common_benchmark_suite/openxla/benchmark/comparative_suite/jax/model_definitions.py
+++ common_benchmark_suite/openxla/benchmark/comparative_suite/jax/model_definitions.py
@@ -10,7 +10,7 @@ import string
 from openxla.benchmark import def_types
 from openxla.benchmark.comparative_suite import utils
 
-PARENT_GCS_DIR = "https://storage.googleapis.com/iree-model-artifacts/jax/jax_models_0.4.20_1699872537"
+PARENT_GCS_DIR = "https://storage.googleapis.com/iree-model-artifacts/jax/jax_models_0.4.23_1702848353"
 ARTIFACTS_DIR_URL_TEMPLATE = string.Template(PARENT_GCS_DIR + "/${name}")
 
 T5_JAX_IMPL = def_types.ModelImplementation(
@@ -572,4 +572,5 @@ ALL_MODELS = list(
     )) + [
         GPT2LMHEAD_PIPELINE_JAX_1X4XI32,
         T5_SMALL_FP32_JAX_1X128XI32,
+        VIT_CLASSIFICATION_JAX_3X224X224XF32,
     ]
--- experimental/iree/compile_workloads.py
+++ experimental/iree/compile_workloads.py
@@ -66,7 +66,7 @@ COMPILE_FLAGS_PER_TARGET = {
     ],
     "pixel-8-pro": [
         "--iree-llvmcpu-target-triple=aarch64-none-linux-android34",
-        "--iree-llvmcpu-target-cpu-features=+fp-armv8,+neon,+crc,+lse,+rdm,+ras,+rcpc,+dotprod,+v9a,+sb,+ssbs,+fullfp16,+fp16fml,+i8mm,+bf16,+flagm"
+        "--iree-llvmcpu-target-cpu-features=+v9a,+fullfp16,fp-armv8,+neon,+aes,+sha2,+crc,+lse,+rdm,+complxnum,+rcpc,+sha3,+sm4,+dotprod,+fp16fml,+dit,+flagm,+ssbs,+sb,+sve2-aes,+sve2-bitperm,+sve2-sha3,+sve2-sm4,+altnzcv,+fptoint,+bf16,+i8mm,+bti,+mte,+pauth,+perfmon,+predres,+spe,+ras"
     ],
 }
 
